8d23300647201c57a06a3a4dab2778f66783e2d3
IT21208508_Darshi <99190916+IT21208508@users.noreply.github.com>
2022-03-16 14:14:43 +0000

    tute6 ex 1 completed

--- Student.h
+++ Student.h
@@ -2,7 +2,14 @@ class Student {
   // private section
   //    int studentId
   //    name <- 20 charcters
-  
+  private:
+  int studentId;
+  char name[20];
+
+public:
+
+  void assignDetails(int sId,const char sName[]);
+  void display();
   // public section
   //    assignDetails() method declaration
   //    display() method declaration
